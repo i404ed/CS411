99f9a0830446766ffb9e25fba8b4129120a918ac
prinnb <prinn_band94@hotmail.com>
2014-03-20 01:29:00 -0500

    edit

--- editAccount.js
+++ editAccount.js
@@ -1,23 +1,7 @@
 $(document).ready(function() {
     //toggle `popup` / `inline` mode
-    $.fn.editable.defaults.mode = 'inline';     
+    $.fn.editable.defaults.mode = 'popup';      
     
     $('#edit_username').editable();
-    $('#edit_email').editable();
-    $('#edit_password').editable();
-    $('#edit_major').editable();
-    
-    /*sample*/
-    $('#edit_standing').editable({
-        type: 'select',
-        title: 'Select status',
-        placement: 'right',
-        value: 2,
-        source: [
-            {value: 1, text: 'status 1'},
-            {value: 2, text: 'status 2'},
-            {value: 3, text: 'status 3'}
-        ]
-    });
-    });
+
 });
