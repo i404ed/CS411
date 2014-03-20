cfc6d9165240734691e3fa4a34515e0b3ae78dc2
i404ed <harlancao@yahoo.com>
2014-03-19 23:07:19 -0500

    so many duplicates. fixed debugging

--- SoupParser.py
+++ SoupParser.py
@@ -9,6 +9,7 @@ from itertools import izip
 # f = open("/home/ext/cs411/project/cs411.html", 'r').read()
 
 
+# noinspection PyBroadException
 class parser:
     def __init__(self, file):
         f = open(file, 'r')
@@ -45,27 +46,25 @@ class parser:
 
         root = self.soup.find("div", class_="portlet-content-inner")
         # print root
-        # title = root.find_all("p", class_="cis-section-title")
-        # print title[0].contents[0].strip()
 
         # course title, course name, credit hrs, description, same as, prereqs
         try:
             title = root.find_all("p", class_="cis-section-title")
-            # print title[0].contents[0].strip().encode('utf-8')
+            print title[0].contents[0].strip().encode('utf-8')
             course = root.find_all("p", class_="cis-section-course")
-            # print course[0].contents[0].strip().encode('utf-8')
+            print course[0].contents[0].strip().encode('utf-8')
             subject_infos = root.find_all("div", id=re.compile("^subject-info"))
             descendants = subject_infos[0].find_all("p", class_="portlet-padtop10")
             credit_hr = descendants[0]
-            # print credit_hr.contents[1].contents[0].strip().encode('utf-8')
-            # print credit_hr.contents[2].strip().encode('utf-8')
+            print credit_hr.contents[1].contents[0].strip().encode('utf-8')
+            print credit_hr.contents[2].strip().encode('utf-8')
             descript = descendants[1]
-            # print descript.contents[0].strip().encode('utf-8')
+            print descript.contents[0].strip().encode('utf-8')
             # descendants 2 doesnt exist? template is sort of messed up. 373 vs 125 + 473
             # sometimes prereq is build into description
             # cross list is built into description
             prereq = descendants[2]
-            # print prereq.contents[0].strip().encode('utf-8')
+            print prereq.contents[0].strip().encode('utf-8')
         except:
             pass
             # print "Course Not Found"
@@ -75,7 +74,7 @@ class parser:
         try:
             misc = subject_infos[1].find_all("p", class_="portlet-padtop10")
             descendants = subject_infos[1].find_all("p", class_="portlet-padtop10")
-            # print misc[0].contents[0].strip().encode('utf-8')
+            print misc[0].contents[0].strip().encode('utf-8')
         except:
             pass
             # print "Misc Not Found"
@@ -84,9 +83,9 @@ class parser:
         # UIUC: Quant Reasoning II course
         try:
             extra = subject_infos[1].find_next_sibling("p", class_="portlet-padtop10")
-            # print extra.contents[1].contents[0].strip().encode('utf-8')
-            # print extra.contents[1].contents[1].contents[0].strip().encode('utf-8')
-            # print extra.contents[4].strip().encode('utf-8')
+            print extra.contents[1].contents[0].strip().encode('utf-8')
+            print extra.contents[1].contents[1].contents[0].strip().encode('utf-8')
+            print extra.contents[4].strip().encode('utf-8')
         except:
             pass
             # print "Extra Not Found"
@@ -100,8 +99,6 @@ class parser:
             table_entrys = table[0].find_all("tr", class_=re.compile(r"^table-item$"))
             table_entrys_info = table[0].find_all("tr", class_=re.compile(r"^table-item-detail"))
             assert len(table_entrys) == len(table_entrys_info)
-            # print len(table_entrys)
-            # print len(table_entrys_info)
             # length of the 2 should be the same
             for entry, info in izip(table_entrys, table_entrys_info):
                 icon0 = entry.find_all("td", class_="w50")[0]
@@ -109,16 +106,10 @@ class parser:
                 print crn.contents[1].contents[0].strip().encode('utf-8')
 
                 types = entry.find_all("td", class_="w80")[0]
-                # type = entry.find_all("td", class_="w80")
-                # print type.contents[1].contents[0].strip().encode('utf-8')
                 section = entry.find_all("td", class_="w55")[0]
-                # print section.contents[2].strip().encode('utf-8')
                 time = entry.find_all("td", class_="w75")[0]
-                # print time.contents[1].contents[0].strip().encode('utf-8')
                 days = entry.find_all("td", class_="w55")[1]
-                # print days.contents[1].contents[0].strip().encode('utf-8')
                 location = entry.find_all("td", class_="w120")[0]
-                # print location.contents[1].contents[0].strip().encode('utf-8')
                 instructors = entry.find_all("td", class_=re.compile(r"ie-table-width"))[0]
 
                 # takes care of paired classes like cs398
