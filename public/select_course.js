8ee81d959ab0742f9f9c6907d743462bbad6ac7a
Haotian Jiang <infijht@gmail.com>
2014-04-26 16:39:31 -0500

    second

--- public/select_course.js
+++ public/select_course.js
@@ -10,3 +10,23 @@ $( "#flip2" ).click(function() {
     alert( "Course add or drop." );
   });
 });
+
+
+//here comes the funciton to search and return the result;
+
+
+  function search_course(){
+   var searchvalue = document.getElementById('search_courses').value;
+   var from_s = document.getElementById('select_course');
+   for(var i =0; i<from_s.options.length;i++)
+   {
+	var temp = from_s.options[i].text;
+        var exist = temp.search(searchvalue);
+	if(exist >=1)
+	{	
+ 		alert("add course");
+	}
+
+   }
+
+	}
