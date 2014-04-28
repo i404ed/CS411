23f4e68c1d42936a37f4e04da18c5a37d62105b5
prinnb <prinn_band94@hotmail.com>
2014-04-28 18:57:04 -0500

    add course to calendar with local sample data

--- public/trigger.js
+++ public/trigger.js
@@ -2,7 +2,6 @@
  * Created by ztx on 3/20/14.
  */
 
-
 var oururl="172.16.159.124";
 var courselist_json = [{"label":"CS 242"}];
 var lasttime = "";
@@ -25,6 +24,21 @@ var samplecoursedetail2 = {
         ]
 };
 
+var sampleharleninput = [
+        {"CourseID":"cs411", 
+        "Sections": [
+             {"Section" : "AL1", "Time" : "01:00 PM - 01:50 PM\n02:00 PM - 02:50 PM", "Days":"M\nM", "Type":"Discussion/Recitation\nLaboratory", "Availability" : "close"}
+        ]},
+
+        {"CourseID":"cs421", 
+        "Sections": [
+            {"Section" : "AL6", "Time" : "03:00 PM - 03:50 PM", "Days":"F", "Type":"Lecture", "Availability" : "close"},
+            {"Section" : "AL5", "Time" : "10:00 AM - 01:50 PM", "Days":"TR", "Type":"Discussion/Recitation", "Availability" : "close"}
+        ]
+        }
+];
+
+var sampleharlenoutput = [ { Section: 'AL1', Time: '01:00 PM - 01:50 PM\n02:00 PM - 02:50 PM', Days: 'M\nM', Type: 'Discussion/Recitation\nLaboratory', Availability: 'open', CourseID: 'cs411' }, { Section: 'AL6', Time: '03:00 PM - 03:50 PM', Days: 'F', Type: 'Lecture', Availability: 'open', CourseID: 'cs421' }, { Section: 'AL100', Time: '10:00 AM - 01:50 PM', Days: 'TR', Type: 'Discussion/Recitation', Availability: 'open', CourseID: 'cs421' } ];
 
 function requestcourse(){
     var searchvalue = document.getElementById('course_search').value;
@@ -62,8 +76,6 @@ function requestcourse(){
 }
 
 $(document).ready(function () {
-
-
 
     var email = sessionStorage.getItem('email');
     var pwd = sessionStorage.getItem('pwd');
@@ -134,44 +146,36 @@ $(document).ready(function () {
             });
 
         }
-        /*
-        selectedcourses.push(samplecoursedetail);
-
-        var courseid = samplecoursedetail["CourseID"];
-        var sections = samplecoursedetail.Sections;
-        var currhtml = '<li id =' + courseid + '> <h5>' + courseid +'</h5>';
-        currhtml += '<button type="button" class = "coursedelete" id = '+ courseid + '_deletebutton' +'>Delete</button>';
-
-        currhtml += '<table style="width:100%;" class = "section_table" id ='+ courseid + '_table>' ;
-        for (var i = 0; i < sections.length; i++) {
-            currhtml += '<tr class = "coursedeletess" id ='+ sections[i].Section +'><td>' + sections[i].Section + '</td><td>' + sections[i].CRN + '</td><td>' + sections[i].Days.replace("\n","/") + '</td><td>'+ sections[i].Time.replace("\n","/") + '</td><td>'+ sections[i].Availability + '</td></tr>';
-        }
-        currhtml += '</table>';
-        currhtml += '</li>';
-        $("#course_detail_list").append(currhtml);
-
-
-        selectedcourses.push(samplecoursedetail2);
-
-        var courseid = samplecoursedetail2["CourseID"];
-        var sections = samplecoursedetail2.Sections;
-        var currhtml = '<li id =' + courseid + '> <h5>' + courseid +'</h5>';
-        currhtml += '<button type="button" class = "coursedelete" id = '+ courseid + '_deletebutton' +'>Delete</button>';
-
-        currhtml += '<table style="width:100%;" class = "section_table" id ='+ courseid + '_table>';
-        for (var i = 0; i < sections.length; i++) {
-            currhtml += '<tr class = "coursedeletess" id ='+ sections[i].Section +'><td>' + sections[i].Section + '</td><td>' + sections[i].CRN + '</td><td>' + sections[i].Days.replace("\n","/") + '</td><td>'+ sections[i].Time.replace("\n","/") + '</td><td>'+ sections[i].Availability + '</td></tr>';
-        }
-        currhtml += '</table>';
-        currhtml += '</li>';
-        $("#course_detail_list").append(currhtml);
-        */
 
     });
 
     $(document).on('click', "#generate",function() {
-        //send to harlen
-        alert(selectedcourses);
+        //call harlen function
+        //send to ztx
+        for (var k = 0; k < sampleharlenoutput.length; k++){
+            var courseid = sampleharlenoutput[k].CourseID;
+            var section = sampleharlenoutput[k].Section;
+            var days = sampleharlenoutput[k].Days.replace("\n","/");
+            var time = sampleharlenoutput[k].Time.replace("\n","/");
+            alert(time);
+            convert_time_add_calendar(courseid, section, days, time);
+        }
+
+        /*
+        for (var k = 0; k < sampleharlenoutput.length; k++){
+            var courseid = sampleharlenoutput[k].CourseID;
+            var sections = sampleharlenoutput[k].Sections;
+            alert(sections.length);
+            for (var m = 0; m < sections.length; m++) {
+                var section = sections[m].Section;
+                var days = sections[m].Days.replace("\n","/");
+                var time = sections[m].Time.replace("\n","/");
+                alert(time);
+                convert_time_add_calendar(courseid, section, days, time);
+
+            }
+        }
+        */
     });
 
     $(document).on('click', ".coursedelete",function() {
@@ -181,8 +185,5 @@ $(document).ready(function () {
         selectedcourses.splice(listindex, 1);
         $('#' + listelemid).remove();
     });
-
-
-
 
 });
